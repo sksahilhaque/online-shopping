9b545a57f3fc7933cda14054b73a9dca360e6e59
Sahil <sksahilhaque@gmail.com>
2019-08-22 14:11:13 +0530

    Completed the interface and other issues!

--- onlineshopping/src/main/java/net/sk/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/sk/onlineshopping/controller/PageController.java
@@ -2,7 +2,6 @@ package net.sk.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -13,27 +12,27 @@ public class PageController {
 	public  ModelAndView index()
 	{
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting","Welcome to Spring Web MVC");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
 		return mv;
 	}
 	
-	@RequestMapping(value="/test")
-	public ModelAndView test(@RequestParam(value="greeting", required=false)String greeting)
+	@RequestMapping(value = "/about")
+	public  ModelAndView about()
 	{
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting",greeting);
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);
 		return mv;
 	}
 	
-//	@RequestMapping(value="/test/{greeting}")
-//	public ModelAndView test(@PathVariable("greeting")String greeting)
-//	{
-//		if(greeting == null)
-//		{
-//			greeting = "Hello";
-//		}
-//		ModelAndView mv = new ModelAndView("page");
-//		mv.addObject("greeting",greeting);
-//		return mv;
-//	}
+	@RequestMapping(value = "/contact")
+	public  ModelAndView contact()
+	{
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", true);
+		return mv;
+	}
+		
 }
